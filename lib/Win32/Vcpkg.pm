9bfaa3490c3795363c6dd5a8e36ca7b183577993
Graham Ollis <plicease@cpan.org>
2020-03-05 06:52:00 -0700

    Initial structure

---
+++ lib/Win32/Vcpkg.pm
@@ -0,0 +1,12 @@
+package Win32::Vcpkg;
+
+use strict;
+use warnings;
+use 5.008001;
+
+# ABSTRACT: Interface to Microsoft Vcpkg
+# VERSION
+
+1;
+
+
---
+++ t/win32_vcpkg.t
@@ -0,0 +1,8 @@
+use Test2::V0 -no_srand => 1;
+use Win32::Vcpkg;
+
+ok 1, 'todo';
+
+done_testing;
+
+
